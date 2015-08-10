c321cf50bfb3e245b9e300ec9cce2451e34d6d34
Windmolders <jonas_windmolders@hotmail.com>
2015-08-10 13:45:38 +0200

    new system.

--- public/js/jarys.js
+++ public/js/jarys.js
@@ -1,12 +1,14 @@
 $(document).ready(function ($) {
 
-  function changeTopContect(content) {
-    $('.top-info').html(content);
+  function changeTopContent(content) {
+    $('.top-info').load('../resources/views/subviews/' + content);
     $(".top-info").customScrollbar();
   }
 
-  function changeBotContect(content) {
-    $('.bot-info').html(content);
+
+
+  function changeBotContent(content) {
+    $('.bot-info').load('../resources/views/subviews/' + content);
     $(".bot-info").customScrollbar();
   }
 
@@ -19,15 +21,17 @@ $(document).ready(function ($) {
     if(images.length > 0) {
 
       images.forEach(function (entry) {
-        html += '<img class="side-img" src="../public/jarys/dp/folders/' + entry + '" /><p><br/>test</p>';
+        html += '<img class="side-img" src="../public/jarys/dp/folders/' + entry + '" /><p>&nbsp;</p>';
       });
 
-      changeBotContect(html);
+      $('.bot-info').html(html);
+      $(".bot-info").customScrollbar();
     }
   }
 
   changeBotImages();
-  changeTopContect('<p>Vestibulum id tortor sit amet velit luctus gravida eget ut felis. Duis a mollis nisl, at tristique odio. Nunc cursus quam augue, vel accumsan nisl luctus quis. Mauris id sapien vehicula, sollicitudin augue accumsan, posuere nisl. Suspendisse semper leo eget aliquet hendrerit. Donec suscipit felis aliquam, auctor odio nec, cursus magna. Nullam in est tortor. Fusce eget est sit amet lorem maximus facilisis. Duis commodo ligula et felis lobortis dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis est enim, finibus quis lobortis fringilla, suscipit non mauris.In semper eu urna eu lobortis. Fusce non eros at tortor facilisis posuere id eu neque. In hac habitasse platea dictumst. Curabitur a volutpat risus. Nunc eget enim efficitur, vulputate justo at, ornare turpis. Etiam et lobortis est. Integer id ex non ex porttitor pulvinar. </p>');
+
+  changeTopContent('dp/folder.html');
 
 
 });
